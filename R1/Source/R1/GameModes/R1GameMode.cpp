569ed14cf2fa67d8c8b450a7516431af24168355
mangwonKing <aud1035@naver.com>
2023-12-27 12:11:59 +0900

    GameMode Create Study

---
+++ R1/Source/R1/GameModes/R1GameMode.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameModes/R1GameMode.h"
+
+AR1GameMode::AR1GameMode(const FObjectInitializer& ObjectInitializer)
+	:	Super(ObjectInitializer)
+{
+
+}
---
+++ R1/Source/R1/GameModes/R1GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "R1GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class R1_API AR1GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	AR1GameMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
+};
